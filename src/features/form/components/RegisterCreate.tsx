7c83745664264c124f03cc4844ad54035989a4de
Andres Arrieche <andresarrieche11@gmail.com>
2024-07-08 16:48:09 -0400

    added some styles for compatibility

--- src/features/form/components/RegisterCreate.tsx
+++ src/features/form/components/RegisterCreate.tsx
@@ -1,133 +1,136 @@
-import { useState } from 'react'
-import { ContainerForm } from '././small_components/ContainerForm'
-import { PersonalInfoForm } from './PersonalInfoForm'
-import { ContactInfoForm } from './ContactInfoForm'
-import { AcademicCareer } from './AcademicCareer'
-import { ProfessionalCareer } from './ProfessionalCareer'
-import { ButtonNextBackForm } from '././small_components/ButtonNextBackForm'
-import { SocialMediaForm } from './SocialMediaForm'
-import {contactInfoProps, SocialMediaProps, seminarianInfo, professionalInfo} from '../interfaces/Form'
-import { personalInfoProps } from '../interfaces/Form'
+import { useState } from 'react';
+import { ContainerForm } from '././small_components/ContainerForm';
+import { PersonalInfoForm } from './PersonalInfoForm';
+import { ContactInfoForm } from './ContactInfoForm';
+import { AcademicCareer } from './AcademicCareer';
+import { ProfessionalCareer } from './ProfessionalCareer';
+import { ButtonNextBackForm } from '././small_components/ButtonNextBackForm';
+import { SocialMediaForm } from './SocialMediaForm';
+import {
+	contactInfoProps,
+	SocialMediaProps,
+	seminarianInfo,
+	professionalInfo,
+} from '../interfaces/Form';
+import { personalInfoProps } from '../interfaces/Form';
+import { ContentContainer } from '../../ui/container/components/ContentContainer';
 const RegisterCreate = () => {
-    const [number, setNumber]=useState(1);
-    const [modal, setModal]=useState(false);
-    const rol= 'seminarista';
+	const [number, setNumber] = useState(1);
+	const [modal, setModal] = useState(false);
+	const rol = 'seminarista';
 
-    const [personalInfo,setPersonalInfo]= useState<personalInfoProps>({
-        name: '',
-        lastName: '',
-        id: '',
-        birthDate: '',
-        bloodType:'',
-        medicalRecord:'',
-        rol:''
-    })
+	const [personalInfo, setPersonalInfo] = useState<personalInfoProps>({
+		name: '',
+		lastName: '',
+		id: '',
+		birthDate: '',
+		bloodType: '',
+		medicalRecord: '',
+		rol: '',
+	});
 
-    const [contactInfo,setContactInfo]=useState<contactInfoProps>({
-        phone: '',
+	const [contactInfo, setContactInfo] = useState<contactInfoProps>({
+		phone: '',
 		phoneFamily: '',
 		descriptionFamily: '',
-		email: ''
-    })
-    const [socialMedia, setSocialMedia] = useState<SocialMediaProps[]>([])
-    const [seminarianInfo, setSeminarianInfo] = useState<seminarianInfo>({
-        academicTraining: '',
-        stage:'',
-        linkTitle:'',
-        diocese:'',
-        parish: '',
-        apostolates:'',
-        ministriesReceived:'',
-        condition:'',
-        status:'',
-        nameSeminaryExternal:'',
-        yearOfIncome:'',
-    })
+		email: '',
+	});
+	const [socialMedia, setSocialMedia] = useState<SocialMediaProps[]>([]);
+	const [seminarianInfo, setSeminarianInfo] = useState<seminarianInfo>({
+		academicTraining: '',
+		stage: '',
+		linkTitle: '',
+		diocese: '',
+		parish: '',
+		apostolates: '',
+		ministriesReceived: '',
+		condition: '',
+		status: '',
+		nameSeminaryExternal: '',
+		yearOfIncome: '',
+	});
 
-    const [professionalInfo,setProfessionalInfo]=useState<professionalInfo>({
-        academicTraining:'',
-        linkTitle:'',
-        ordinationDate:'',
-        ministryYears:''
-    })
-    return (
-        <ContainerForm>
-            
-            <form action="POST">
-                <div>
-                    {
-                    number === 1 ? (
-                        <>
-                            <PersonalInfoForm
-                                name={personalInfo.name}
-                                lastName={personalInfo.lastName}
-                                id={personalInfo.id}
-                                birthDate={personalInfo.birthDate}
-                                bloodType={personalInfo.bloodType}
-                                medicalRecord={personalInfo.medicalRecord}
-                                rol={personalInfo.rol}
-                                setPersonalInfo={setPersonalInfo}
-                            />
-                            <ButtonNextBackForm initial setNumber={setNumber}/>
-                        </>
-                    ) : number === 2 ? (
-                        <>
-                            <ContactInfoForm
-                                contactInfo={contactInfo}
-                                setContactInfo={setContactInfo}
-                            
-                            />
-                            <ButtonNextBackForm setNumber={setNumber}/>
-                        </>
-                    ) : number === 3 ? (
-                        <>
-                            <SocialMediaForm
-                                setModal={setModal}
-                                modal={modal}
-                                socialMedia={socialMedia}
-                                setSocialMedia={setSocialMedia}
-                            />
-                            <ButtonNextBackForm setNumber={setNumber}/>
-                        </>
+	const [professionalInfo, setProfessionalInfo] = useState<professionalInfo>({
+		academicTraining: '',
+		linkTitle: '',
+		ordinationDate: '',
+		ministryYears: '',
+	});
+	return (
+		<ContentContainer>
+			<form action="POST">
+				{number === 1 ? (
+					<>
+						<PersonalInfoForm
+							name={personalInfo.name}
+							lastName={personalInfo.lastName}
+							id={personalInfo.id}
+							birthDate={personalInfo.birthDate}
+							bloodType={personalInfo.bloodType}
+							medicalRecord={personalInfo.medicalRecord}
+							rol={personalInfo.rol}
+							setPersonalInfo={setPersonalInfo}
+						/>
+						<ButtonNextBackForm initial setNumber={setNumber} />
+					</>
+				) : number === 2 ? (
+					<>
+						<ContactInfoForm
+							contactInfo={contactInfo}
+							setContactInfo={setContactInfo}
+						/>
+						<ButtonNextBackForm setNumber={setNumber} />
+					</>
+				) : number === 3 ? (
+					<>
+						<SocialMediaForm
+							setModal={setModal}
+							modal={modal}
+							socialMedia={socialMedia}
+							setSocialMedia={setSocialMedia}
+						/>
+						<ButtonNextBackForm setNumber={setNumber} />
+					</>
+				) : number === 4 ? (
+					<>
+						{rol === 'seminarista' ? (
+							<AcademicCareer
+								academicTraining={
+									seminarianInfo.academicTraining
+								}
+								stage={seminarianInfo.stage}
+								linkTitle={seminarianInfo.linkTitle}
+								diocese={seminarianInfo.diocese}
+								parish={seminarianInfo.parish}
+								apostolates={seminarianInfo.apostolates}
+								ministriesReceived={
+									seminarianInfo.ministriesReceived
+								}
+								condition={seminarianInfo.condition}
+								status={seminarianInfo.status}
+								nameSeminaryExternal={
+									seminarianInfo.nameSeminaryExternal
+								}
+								yearOfIncome={seminarianInfo.yearOfIncome}
+								setSeminarianInfo={setSeminarianInfo}
+							/>
+						) : (
+							<ProfessionalCareer
+								academicTraining={
+									professionalInfo.academicTraining
+								}
+								linkTitle={professionalInfo.linkTitle}
+								ordinationDate={professionalInfo.ordinationDate}
+								ministryYears={professionalInfo.ministryYears}
+								setProfessionalInfo={setProfessionalInfo}
+							/>
+						)}
+						<ButtonNextBackForm setNumber={setNumber} />
+					</>
+				) : null}
+			</form>
+		</ContentContainer>
+	);
+};
 
-                    ): number ===4 ?(
-                        <>
-                            { rol === 'seminarista' ?
-                                (   
-                                    <AcademicCareer
-                                        academicTraining={seminarianInfo.academicTraining}
-                                        stage={seminarianInfo.stage}
-                                        linkTitle={seminarianInfo.linkTitle}
-                                        diocese={seminarianInfo.diocese}
-                                        parish={seminarianInfo.parish}
-                                        apostolates={seminarianInfo.apostolates}
-                                        ministriesReceived={seminarianInfo.ministriesReceived}
-                                        condition={seminarianInfo.condition}
-                                        status={seminarianInfo.status}
-                                        nameSeminaryExternal={seminarianInfo.nameSeminaryExternal}
-                                        yearOfIncome={seminarianInfo.yearOfIncome}
-                                        setSeminarianInfo={setSeminarianInfo}
-                                        
-                                    />
-                                ):
-                                    <ProfessionalCareer
-                                        academicTraining={professionalInfo.academicTraining}
-                                        linkTitle={professionalInfo.linkTitle}
-                                        ordinationDate={professionalInfo.ordinationDate}
-                                        ministryYears={professionalInfo.ministryYears}
-                                        setProfessionalInfo={setProfessionalInfo}
-                                    />
-                                
-                            }
-                            <ButtonNextBackForm setNumber={setNumber}/>
-                            
-                        </>
-                    ):
-                    null}
-                </div>
-            </form>
-        </ContainerForm>
-    )
-}
-
-export default RegisterCreate
+export default RegisterCreate;
--- src/pages/RegisterForm.tsx
+++ src/pages/RegisterForm.tsx
@@ -1,14 +1,21 @@
-import ContentTitle from '../features/ui/contentTitle/components/ContentTitle'
-import RegisterCreate from '../features/form/components/RegisterCreate'
-import FormCSS from '../features/form/styles/FormCSS.module.css'
+import ContentTitle from '../features/ui/contentTitle/components/ContentTitle';
+import RegisterCreate from '../features/form/components/RegisterCreate';
+import FormCSS from '../features/form/styles/FormCSS.module.css';
+import { TitleList } from '../features/ui/title/components/TitleList';
+import { Title } from '../features/ui/title/components/Title';
+import { BackgroundColoredSubtitle } from '../features/ui/title/components/BackgroundColoredSubtitle';
 
 const RegisterForm = () => {
-    return (
-        <div className={FormCSS['registerForm']}>
-            <ContentTitle title='Profesor' subtitle='Agregar Profesor'/>
-            <RegisterCreate/>
-        </div>
-    )
-}
+	return (
+		<>
+			<TitleList>
+				<Title content="Profesor" />
+				<BackgroundColoredSubtitle content="Agregar Profesor" />
+			</TitleList>
+			{/* <ContentTitle title='Profesor' subtitle='Agregar Profesor'/> */}
+			<RegisterCreate />
+		</>
+	);
+};
 
-export default RegisterForm
+export default RegisterForm;
