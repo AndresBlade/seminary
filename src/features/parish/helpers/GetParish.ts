f0de644525118c9528d0c504aded1a18befb28ba
Andres Arrieche <andresarrieche11@gmail.com>
2024-07-16 11:46:07 -0400

    Fixed issues with interfaces not resembling the real data from back

--- src/features/parish/helpers/GetParish.ts
+++ src/features/parish/helpers/GetParish.ts
@@ -1,12 +1,12 @@
-import { ParishDataContentPropss } from "../../../pages/ParishShowData";
+import { ParishDataContentPropss } from '../../../pages/ParishShowData';
 
-interface Parish{
-    msj: string,
-    parishrepository: ParishDataContentPropss[];
+interface Parish {
+	msj: string;
+	parishrepository: ParishDataContentPropss[];
 }
 
-export const GetParish = ():Promise<Parish[]>=>{
-    return fetch('http://127.0.0.1:3000/parish').then(
-        response => response.json() as Promise<Parish[]>
-    )
-}
+export const GetParish = (): Promise<Parish> => {
+	return fetch('http://127.0.0.1:3000/parish').then(
+		response => response.json() as Promise<Parish>
+	);
+};
--- src/features/parish/helpers/GetParishByName.ts
+++ src/features/parish/helpers/GetParishByName.ts
@@ -1,16 +1,16 @@
-interface Parish{
-    id: number;
-    diocese_id: number;
-    name: string;
-    patron: string;
+interface Parish {
+	id: number;
+	diocese_id: number;
+	name: string;
+	patron: string;
 }
-interface parishWrapper{
-    msj: string;
-    parish: Parish[];
+interface parishWrapper {
+	msj: string;
+	parish: Parish[];
 }
 
-export const GetParishByName =({name}:{name:string})=>{
-    return fetch(`http://localhost:3000/parish/search/${name}`).then(
-        response => response.json() as Promise<parishWrapper>
-    ).then(data => data.parish)
-}
+export const GetParishByName = ({ name }: { name: string }) => {
+	return fetch(`http://localhost:3000/parish/search/${name}`).then(
+		response => response.json() as Promise<parishWrapper>
+	);
+};
