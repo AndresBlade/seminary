e522b11a8eef300b12c82ffe2cb422ab204bba3c
Andres Arrieche <andresarrieche11@gmail.com>
2024-08-25 17:54:36 -0400

    fixed role permissions table

--- src/features/roles/components/PermissionsTable.tsx
+++ src/features/roles/components/PermissionsTable.tsx
@@ -1,51 +1,84 @@
 import { CheckedPermission } from '../interfaces/CheckedPermission';
 import { Permission } from '../interfaces/Permission';
-import RolesCSS from '../styles/roles.module.css';
-import { Routes } from './Routes';
-import { Row } from './Row';
+import TableCSS from '../../subject/styles/Table.module.css';
+import { TableColumn } from '../../subject/components/TableColumn';
+import { Field } from './Field';
 
 interface Props {
 	tables: string[];
-	permissionsByRoute: Record<string, Permission[]>;
 	checkedPermissions: CheckedPermission[];
 	setCheckedPermissions: React.Dispatch<
 		React.SetStateAction<CheckedPermission[] | null>
 	>;
+	permissions: Permission[];
 }
+
+const groupPermissionsByType = (permissions: Permission[]) =>
+	permissions.reduce(
+		(group: Record<string, Permission[]>, permission: Permission) => {
+			const { type } = permission;
+
+			if (!group[type]) {
+				group[type] = group[type] ?? [];
+			}
+			group[type].push(permission);
+			return group;
+		},
+		{}
+	);
 
 export const PermissionsTable = ({
 	tables,
-	permissionsByRoute,
 	checkedPermissions,
 	setCheckedPermissions,
+	permissions,
 }: Props) => {
+	const permissionsByType = groupPermissionsByType(permissions);
+	const classNames = tables.map((_, index) =>
+		index % 2 === 1 ? TableCSS.cell : TableCSS.deactivatedCell
+	);
 	return (
-		<div className={RolesCSS.table}>
-			<Routes tables={tables} />
-			<div className={RolesCSS.permissions}>
-				<div className={RolesCSS.headers}>
-					<p>Acceso Total</p>
-					<p>Ver</p>
-					<p>Crear</p>
-					<p>Editar</p>
-					<p>Eliminar</p>
-				</div>
-				<div className={RolesCSS.rows}>
-					{tables?.map(
-						(table, index) =>
-							permissionsByRoute && (
-								<Row
-									checkedPermissions={checkedPermissions}
-									setCheckedPermissions={
-										setCheckedPermissions
-									}
-									key={index}
-									permissions={permissionsByRoute[table]}
-								/>
-							)
-					)}
-				</div>
-			</div>
+		<div className={TableCSS.table}>
+			<TableColumn
+				content={tables}
+				type="content"
+				title="Tabla"
+				classNames={classNames}
+			/>
+			{Object.keys(permissionsByType).map((key, indexOfType) => {
+				const title =
+					key === 'C'
+						? 'Crear'
+						: key === 'U'
+						? 'Editar'
+						: key === 'D'
+						? 'Eliminar'
+						: 'Leer';
+				return (
+					<TableColumn title={title} key={indexOfType} type="element">
+						{tables.map((table, index) => {
+							const permission = permissionsByType[key].find(
+								permission => permission.table === table
+							);
+							return (
+								<div className={classNames[index]} key={index}>
+									{permission && (
+										<Field
+											checkedPermissions={
+												checkedPermissions
+											}
+											permission={permission}
+											setCheckedPermissions={
+												setCheckedPermissions
+											}
+										/>
+									)}
+								</div>
+							);
+						})}
+					</TableColumn>
+				);
+			})}
 		</div>
 	);
 };
