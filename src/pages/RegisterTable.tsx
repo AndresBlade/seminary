82dc049398a0ca74f28b06baa9f7c714c34f932b
Andres Arrieche <andresarrieche11@gmail.com>
2024-08-28 16:02:06 -0400

    fixed colors red passed to orange

--- src/pages/RegisterTable.tsx
+++ src/pages/RegisterTable.tsx
@@ -1,17 +1,17 @@
-import { ShowDataContent } from "../features/form/components/ShowDataContent"
+import { ShowDataContent } from '../features/form/components/ShowDataContent';
 import { TitleList } from '../features/ui/title/components/TitleList';
 import { Title } from '../features/ui/title/components/Title';
 import { BackgroundColoredSubtitle } from '../features/ui/title/components/BackgroundColoredSubtitle';
 const RegisterTable = () => {
-    return (
-        <div>
-            <TitleList>
+	return (
+		<>
+			<TitleList>
 				<Title content="Usuarios" />
 				<BackgroundColoredSubtitle content="Lista de Usuarios" />
 			</TitleList>
-            <ShowDataContent/>
-        </div>
-    )
-}
+			<ShowDataContent />
+		</>
+	);
+};
 
-export default RegisterTable
+export default RegisterTable;
